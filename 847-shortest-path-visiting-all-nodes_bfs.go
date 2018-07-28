5bc8aa985ea1dd4c7cfcbff2503dd79ed37e5d31
Qi Xiao <xiaq@users.noreply.github.com>
2018-07-29 00:38:15 +0100

    Create 847-shortest-path-visiting-all-nodes_bfs.go

---
+++ 847-shortest-path-visiting-all-nodes_bfs.go
@@ -0,0 +1,38 @@
+func shortestPathLength(graph [][]int) int {
+    n := len(graph)
+    if n == 1 {
+        return 0
+    }
+
+    seen := make(map[int]bool)
+
+    allVisited := (1 << uint(n)) - 1
+
+    // Low 16 bits: bitmask for visited nodes
+    // High 16 bits: current node
+    q := make([]int, n)
+    for i := 0; i < n; i++ {
+        q[i] = (1 << uint(i)) | (i << 16)
+        seen[q[i]] = true
+    }
+
+    for d := 0; len(q) > 0; d++ {
+        newq := make([]int, 0, len(q))
+        for _, state := range q {
+            visited, u := state & 0xffff, state >> 16
+            for _, v := range graph[u] {
+                newVisited := visited | (1 << uint(v))
+                if newVisited == allVisited {
+                    return d + 1
+                }
+                newState := newVisited | (v << 16)
+                if !seen[newState] {
+                    seen[newState] = true
+                    newq = append(newq, newState)
+                }
+            }
+        }
+        q = newq
+    }
+    panic("no solution")
+}
